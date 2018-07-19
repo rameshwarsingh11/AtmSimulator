280306311775262d7246df98730b31d88f6a5071
Rameshwar Singh <home-v3@Rameshwar-Singh.local>
2018-07-19 07:36:12 +0530

    updating logic

--- ATMSimulator/src/com/yodlee/atm/AtmSimulator.java
+++ ATMSimulator/src/com/yodlee/atm/AtmSimulator.java
@@ -135,6 +135,7 @@ public class AtmSimulator {
 	private static float[] calculateDispenseCurrencyNotes(float amount) {
 		float currencyNote=availableCurrecy.pop();
 		int currencyNoteCount=(int) (amount/availableCurrecy.pop());
+		//TODO
 		
 		return null;
 	}
